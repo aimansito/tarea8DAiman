f7a3ad6313f59b19278008ed319f59371e747792
Aiman <ahardao1001@g.educaand.es>
2024-05-20 01:10:00 +0200

    c1

---
+++ src/main/java/com/mycompany/tarea8daiman/Tarea8DAiman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea8daiman;
+
+/**
+ *
+ * @author aiman
+ */
+public class Tarea8DAiman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
